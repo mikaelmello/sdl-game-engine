1eb0704d36c9f0c2d7a77fc076cbb61145cda949
Mikael Mello <git@mikaelmello.com>
2019-03-20 17:48:45 -0300

    Finish TileSet implementation

---
+++ src/TileSet.cpp
@@ -0,0 +1,30 @@
+#include "TileSet.hpp"
+#include <iostream>
+
+TileSet::TileSet(int tileWidth, int tileHeight, const std::string& file) 
+ : tileSet(*(new GameObject()), file), tileWidth(tileWidth), tileHeight(tileHeight) {
+    if (tileSet.IsOpen()) {
+        rows = tileSet.GetHeight() / tileHeight;
+        columns = tileSet.GetWidth() / tileWidth;
+    }
+}
+
+void TileSet::RenderTile(unsigned int index, float x, float y) {
+    if (index >= (unsigned)(rows * columns - 1)) {
+        return;
+    }
+
+    int clipX = (index % columns) * tileWidth;
+    int clipY = (index / columns) * tileHeight; 
+
+    tileSet.SetClip(clipX, clipY, tileWidth, tileHeight);
+    tileSet.Render(x, y);
+}
+
+int TileSet::GetTileHeight() {
+    return tileHeight;
+}
+
+int TileSet::GetTileWidth() {
+    return tileWidth;
+}
