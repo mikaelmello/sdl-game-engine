589bf3fcec0d44ac2fbc70dc9838e25af48d9714
Mikael Mello <git@mikaelmello.com>
2019-04-09 13:13:40 -0300

    Add GameData

---
+++ include/GameData.hpp
@@ -0,0 +1,13 @@
+#ifndef GAMEDATA_H
+#define GAMEDATA_H
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                  INCLUDES E DEFINES
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+class GameData {
+    public:
+        static bool playerVictory;
+}
+
+#endif
